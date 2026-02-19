3223c04b523aedbb2833359461862b227f4b8fcf
Lorenzo <renzobustamante87@gmail.com>
2026-02-19 10:55:37 +0100

    file modificati

--- secondo lavoro/script.js
+++ secondo lavoro/script.js
@@ -1,6 +1,7 @@
-let partizioni = [
-    { id: 1, dimensione: 200, processo: null },
-    { id: 2, dimensione: 300, processo: null },
-    { id: 3, dimensione: 250, processo: null },
-    { id: 4, dimensione: 150, processo: null }
-];
+function setup() {
+ let = createCanvas(400, 700);
+}
+
+function draw() {
+ 
+}
